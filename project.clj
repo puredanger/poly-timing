7319bd173b416d055ee6ecd5ffbc0689c548f162
puredanger <alex.miller@cognitect.com>
2016-01-26 22:51:22 -0600

    update for 1.8

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-beta2"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [criterium "0.4.3"]]
   :aot :all
